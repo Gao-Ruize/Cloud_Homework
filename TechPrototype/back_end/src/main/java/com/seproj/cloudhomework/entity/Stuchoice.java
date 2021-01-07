2d302f5da6095a1a4f131db10c42272b904c037c
caoyunqi <630659134@qq.com>
2021-01-07 16:49:25 +0800

    feat: add choice func

--- TechPrototype/back_end/src/main/java/com/seproj/cloudhomework/entity/Stuchoice.java
+++ TechPrototype/back_end/src/main/java/com/seproj/cloudhomework/entity/Stuchoice.java
@@ -1,5 +1,6 @@
 package com.seproj.cloudhomework.entity;
 
+
 import lombok.Data;
 
 import javax.persistence.*;
@@ -8,85 +9,14 @@ import java.util.Date;
 @Data
 @Entity
 @Table(name = "stuchoice")
-public class Stuchoice {
+public class StuChoice {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     int id;
-    int studentid;
-    int choiceid;
-    String answer;
-    Date committime;
+    int studentId;
+    int choiceId;
+    int answer;
+    Date commitTime;
     int grade;
-
-    public Stuchoice(int studentid, int choiceid, String answer, Date committime, int grade) {
-        this.studentid = studentid;
-        this.choiceid = choiceid;
-        this.answer = answer;
-        this.committime = committime;
-        this.grade = grade;
-    }
-
-    public Stuchoice() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getStudentid() {
-        return studentid;
-    }
-
-    public void setStudentid(int studentid) {
-        this.studentid = studentid;
-    }
-
-    public int getChoiceid() {
-        return choiceid;
-    }
-
-    public void setChoiceid(int choiceid) {
-        this.choiceid = choiceid;
-    }
-
-    public String getAnswer() {
-        return answer;
-    }
-
-    public void setAnswer(String answer) {
-        this.answer = answer;
-    }
-
-    public Date getCommittime() {
-        return committime;
-    }
-
-    public void setCommittime(Date committime) {
-        this.committime = committime;
-    }
-
-    public int getGrade() {
-        return grade;
-    }
-
-    public void setGrade(int grade) {
-        this.grade = grade;
-    }
-
-    @Transient
-    String content;
-
-    public String getContent() {return content;}
-
-    public void setContent(String content) {this.content = content;}
-
-    @Transient
-    int courseid;
-    public int getCourseid() {return courseid;}
-    public void setCourseid(int courseid) {this.courseid = courseid;}
 }
